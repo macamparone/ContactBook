4a7b020e120334d7cb01c6b8a9627a7b0ad21a9d
macamparone <72102849+macamparone@users.noreply.github.com>
2020-10-13 11:20:45 -0300

    contactbook

---
+++ ContactBook/ContactsPage.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using ContactBook.Persistence;
+using SQLite;
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+
+namespace ContactBook
+{
+	public partial class ContactsPage : ContentPage
+	{
+
+		private ObservableCollection<Contact> _contacts;
+		private SQLiteAsyncConnection _connection;
+		private bool _isDataLoaded;
+		public void Close_App()
+		{
+			System.Diagnostics.Process.GetCurrentProcess().CloseMainWindow(); // close
+		}
+		public ContactsPage()
+		{
+			InitializeComponent();
+
+			_connection = DependencyService.Get<ISQLiteDb>().GetConnection(); //db
+		}
+
+		protected override async void OnAppearing()
+		{
+			if (_isDataLoaded)
+				return;
+			_isDataLoaded = true;
+			await LoadData();
+			base.OnAppearing();
+		}
+
+		private async Task LoadData()
+		{
+			await _connection.CreateTableAsync<Contact>();
+
+			var contacts = await _connection.Table<Contact>().ToListAsync();
+
+			_contacts = new ObservableCollection<Contact>(contacts);
+			contactsListView.ItemsSource = _contacts;
+		}
+
+		async void OnAddContact(object sender, System.EventArgs e)
+		{
+			var page = new ContactDetailPage(new Contact());
+
+			page.ContactAdded += (source, contact) =>
+			{
+				_contacts.Add(contact);
+			};
+
+			await Navigation.PushAsync(page);
+		}
+
+		async void OnContactSelected(object sender, Xamarin.Forms.SelectedItemChangedEventArgs e)
+		{
+			if (contactsListView.SelectedItem == null)
+				return;
+
+			var selectedContact = e.SelectedItem as Contact;
+
+			contactsListView.SelectedItem = null;
+
+			var page = new ContactDetailPage(selectedContact);
+			page.ContactUpdated += (source, contact) =>
+			{
+				selectedContact.Id = contact.Id;
+				selectedContact.FirstName = contact.FirstName;
+				selectedContact.LastName = contact.LastName;
+				selectedContact.Phone = contact.Phone;
+				selectedContact.Email = contact.Email;
+				selectedContact.IsBlocked = contact.IsBlocked;
+			};
+
+			await Navigation.PushAsync(page);
+		}
+
+		async void OnDeleteContact(object sender, System.EventArgs e)
+		{
+			var contact = (sender as MenuItem).CommandParameter as Contact;
+
+			if (await DisplayAlert("Warning", $"Are you sure you want to delete {contact.FullName}?", "Yes", "No"))
+			{
+				_contacts.Remove(contact);
+
+				await _connection.DeleteAsync(contact);
+			}
+		}
+        void Exit(object sender, System.EventArgs e)
+		{
+			Close_App();
+		}
+    }
+
+}
---
+++ ContactBook/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using ContactBook.Persistence;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ContactBook
+{
+
+	public partial class MainPage : ContentPage
+	{
+	
+		public MainPage()
+		{
+			InitializeComponent();		
+		}
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+			await Navigation.PushModalAsync(new ContactsPage());
+        }
+    }
+}
